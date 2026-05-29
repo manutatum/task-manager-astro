43e4c1b7b6938815ad7cc40d89824a0396a9cbc8
manutatum <jbmanuel16@gmail.com>
2026-05-29 17:18:47 +0200

    Add Clerk, Tailwind and Node integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import clerk from '@clerk/astro';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import node from '@astrojs/node';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [clerk()],
+
+  adapter: node({
+    mode: 'standalone',
+  }),
+
+  output: 'server'
+});
